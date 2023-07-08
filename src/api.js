c9763726ca1ed6407f3fbba4c3ca83d815420547
willowyoruk <willowyoruk@gmail.com>
2023-07-08 11:39:38 -0500

    Fis delay in API

--- src/api.js
+++ src/api.js
@@ -1,30 +1,26 @@
-// because the API we were using stopped being free
-// we've added this mock fetch method, which supplies
-// its own exchange rate data
 import { fetch } from "./lib/fetch";
 
-// helper to make our exchange rate API call
+// Helper function to fetch exchange rates from the API
 export function getExchangeRates(base, supportedCurrencies) {
   const symbols = supportedCurrencies
-    .filter((symbol) => symbol !== base) // exclude your own code from requested symbols
+    .filter((symbol) => symbol !== base) // Exclude the base currency from the requested symbols
     .join();
   const url = `http://api.exchangeratesapi.io/latest?base=${base}&symbols=${symbols}`;
+
+  // Make the API call using the fetch function
   return fetch(url)
-    .then((res) => res.json())
-    .then(handleAPIErrors)
-    .then((res) => res.rates);
+    .then((res) => res.json()) // Parse the response as JSON
+    .then(handleAPIErrors) // Handle any API errors
+    .then((res) => res.rates); // Return the rates data
 }
 
-// if there was an error log it and return super basic mock data
-// errors for this API came back in the form of { success: false, error: {...}}
-// so we can check for that and log any errors and then respond with a basic
-// valid response so the app doesn't crash
+// Helper function to handle API errors
 function handleAPIErrors(res) {
-  if (res.success) return res;
-  console.error(`Server Error: ${res.error.info}`);
+  if (res.success) return res; // If the API call was successful, return the response
+  console.error(`Server Error: ${res.error.info}`); // Log the error message to the console
   return {
     rates: {
-      USD: 1.0,
+      USD: 1.0, // Provide a fallback response with a basic valid exchange rate
     },
   };
 }
